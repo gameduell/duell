f499137bb17ecd1838564b60060349a40ee0bbce
Rui Campos <rui.campos@gameduell.de>
2014-10-13 12:20:06 +0200

    changing a bit the cross domain xml

--- bin/test_result_listener.py
+++ bin/test_result_listener.py
@@ -51,7 +51,9 @@ class urlhandler:
         return '''<?xml version="1.0"?>
 <!DOCTYPE cross-domain-policy SYSTEM "http://www.adobe.com/xml/dtds/cross-domain-policy.dtd">
 <cross-domain-policy>
-    <allow-access-from domain="*"/>
+    <site-control permitted-cross-domain-policies="all"/>
+    <allow-access-from domain="*" secure="false"/>
+    <allow-http-request-headers-from domain="*" headers="*" secure="false"/>
 </cross-domain-policy>
 '''
 
