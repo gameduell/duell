bc475f5abe11b5c3616c162fe361d0dc09ed7450
Rui Campos <rui.campos@gameduell.de>
2015-01-23 16:11:49 +0100

    added yestoall :(

--- duell/helpers/AskHelper.hx
+++ duell/helpers/AskHelper.hx
@@ -9,14 +9,22 @@ package duell.helpers;
 import duell.helpers.StringHelper;
 
 import duell.helpers.LogHelper;
+import duell.objects.Arguments;
 
 class AskHelper
 {
 	public static function askYesOrNo(question : String) : Bool 
 	{
+
 		while (true) 
 		{
 			LogHelper.print("\x1b[1m" + question + "\x1b[0m \x1b[3;37m[y/n]\x1b[0m? ");
+
+			if (Arguments.isSet("-yestoall"))
+			{
+				LogHelper.println("y");
+				return true;
+			}
 			switch(Sys.stdin().readLine()) 
 			{
 				case "n": return false;
@@ -30,6 +38,13 @@ class AskHelper
 	public static function askString(question : String, defaultResponse : String) : String 
 	{
 		LogHelper.print("\x1b[1m" + question + "\x1b[0m \x1b[3;37m[" + defaultResponse + "]]\x1b[0m? ");
+
+		if (Arguments.isSet("-yestoall"))
+		{
+			LogHelper.println(defaultResponse);
+			return defaultResponse;
+		}
+		
 		var response = Sys.stdin().readLine();
 		response = StringHelper.strip(response);
 
