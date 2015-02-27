8e6ca8696c4a43a188078cda01d0952281f162f1
Rui Campos <rui.campos@gameduell.de>
2015-02-27 12:05:36 +0100

    environment script has to be controlled by expect for some specific things in jenkins

--- ci_environment.sh
+++ ci_environment.sh
@@ -7,10 +7,138 @@ mkdir ~/.duell
 mkdir ~/.duell/haxelib
 haxelib setup ~/.duell/haxelib
 
-neko run.n self_setup -verbose -yestoall
+expect -c "
+spawn neko run.n self_setup -verbose  
+set timeout -1
 
-haxelib run duell setup mac -v 1.0.0 -verbose -yestoall
+expect \"Path to store repos from libraries?\"
+send \"\r\"
 
-haxelib run duell setup flash -v 1.0.0 -verbose -yestoall
 
-haxelib run duell setup android -v 1.0.0 -verbose -yestoall
+expect \"URL to repo list?\"
+send \"\r\"
+
+expect \"Do you want to install the \\\"duell\\\" command?\"
+send \"n\r\"b
+
+expect eof
+"
+
+expect -c "
+spawn haxelib run duell setup mac -verbose
+set timeout -1
+
+expect \"A library for setup of mac environment is not currently installed.\"
+send \"y\r\"
+
+expect eof
+"
+expect -c "
+spawn haxelib run duell setup flash -verbose
+set timeout -1
+
+expect \"A library for setup of flash environment is not currently installed.\"
+send \"y\r\"
+
+expect \"Download and install the Adobe AIR SDK\"
+send \"y\r\"
+expect \"Air SDK Location\"
+send \"\r\"
+
+expect {
+	\"File found\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+
+	\"Go to the flash website\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+
+	\"Download and install the Flash Debugger\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+}
+"
+
+
+
+expect -c "
+spawn haxelib run duell setup android -verbose
+
+set timeout -1
+
+expect \"A library for setup of android environment is not currently installed.\"
+send \"y\r\"
+
+expect \"Download the android SDK\"
+send \"y\r\"
+expect \"Android SDK Location\"
+send \"\r\"
+
+expect {
+	\"File found\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+	\"necessary Android packages\" {
+
+		send \"y\r\"
+	}
+}
+
+expect {
+	\"accept the license\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+
+	\"Download the android NDK\" {
+
+		send \"y\r\"
+	}
+}
+
+expect \"Android NDK Location\"
+send \"\r\"
+
+expect {
+	\"File found\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+
+	\"Download Apache Ant\" {
+
+		send \"y\r\"
+	}
+}
+
+expect \"Apache Ant Location\"
+send \"\r\"
+
+expect {
+	\"File found\" {
+
+		send \"n\r\"
+
+		exp_continue
+	}
+
+}
+"
+
