2c1988d55e0c642a79b8cf11a26e48b8aa9ace04
jxav <joao.xavier@gameduell.de>
2015-03-10 16:13:34 +0100

    Bundling python binary for mac into the duell tool and wrapped the logic for calling python in a PythonProcess

--- duell/helpers/TestHelper.hx
+++ duell/helpers/TestHelper.hx
@@ -5,31 +5,24 @@
  */
 package duell.helpers;
 
-import duell.helpers.PlatformHelper;
 import duell.objects.DuellLib;
-
 import duell.objects.DuellProcess;
+import duell.objects.PythonProcess;
 
-import haxe.io.BytesOutput;
 import haxe.io.Path;
-import haxe.io.Eof;
 import haxe.io.Bytes;
-import haxe.Timer;
 
 import sys.io.File;
-import sys.io.FileInput;
 import sys.io.FileOutput;
-
-import neko.vm.Thread;
 
 class TestHelper
 {
 	public static function runListenerServer(timeout : Float, port : Int, resultOutputPath : String)
 	{
-		var duellLibPath = DuellLib.getDuellLib("duell").getPath();
-		var testProcess = new DuellProcess(
+		var duellLibPath: String = DuellLib.getDuellLib("duell").getPath();
+
+		var testProcess: DuellProcess = new PythonProcess(
 										Path.join([duellLibPath, "bin"]),
-										"python", 
 										["test_result_listener.py", "" + port], 
 										{
 											systemCommand : true, 
---
+++ duell/objects/PythonProcess.hx
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2003-2015 GameDuell GmbH, All Rights Reserved
+ * This document is strictly confidential and sole property of GameDuell GmbH, Berlin, Germany
+ */
+package duell.objects;
+
+import duell.helpers.PlatformHelper;
+import duell.objects.DuellProcess.ProcessOptions;
+import haxe.io.Path;
+
+/**   
+    @author jxav
+ */
+class PythonProcess extends DuellProcess
+{
+    public function new(path: String, args: Array<String>, options: ProcessOptions = null)
+    {
+        var pythonCommand: String = retrievePythonBinary();
+
+        super(path, pythonCommand, args, options);
+    }
+
+    private static function retrievePythonBinary(): String
+    {
+        var duellLibPath: String = DuellLib.getDuellLib("duell").getPath();
+
+        return switch (PlatformHelper.hostPlatform)
+        {
+            case Platform.MAC: Path.join([duellLibPath, "bin", "mac", "python2.7"]);
+            case _: "python";
+        };
+    }
+}
