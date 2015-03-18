b98b7a3304875193bb53bbd562fe49243c92849a
jxav <joao.xavier@gameduell.de>
2015-03-18 18:30:28 +0100

    Attempting to install and use the default hxcpp version

--- duell/commands/UpdateCommand.hx
+++ duell/commands/UpdateCommand.hx
@@ -141,7 +141,7 @@ class UpdateCommand implements IGDCommand
 	private function determineAndValidateDependenciesAndDefines()
 	{
 		/// ADD HXCPP
-		haxelibVersions.set("hxcpp", Haxelib.getHaxelib("hxcpp", DuellDefines.DEFAULT_HXCPP_VERSION));
+        handleHaxelibParsed(Haxelib.getHaxelib("hxcpp", DuellDefines.DEFAULT_HXCPP_VERSION));
 
 		duellToolGitVers = new GitVers(DuellLib.getDuellLib("duell").getPath());
 
