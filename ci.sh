16f25165e76c42521c814ff95e783b2a8f2d744b
Rui Campos <rui.campos@gameduell.de>
2015-04-09 10:43:18 +0200

    put android build in the end for ci

--- ci.sh
+++ ci.sh
@@ -8,14 +8,15 @@ cd test
 
 haxelib run duell create unitTestProject -verbose -yestoall
 
+#because the unitTestProject sample has a specified duell tool version
 sed -i '' 's/supported-duell-tool version=".*"/supported-duell-tool version="master"/g' duell_project.xml
 
 haxelib run duell update -verbose -yestoall
-
-haxelib run duell build android -test -verbose -D jenkins -yestoall
 
 haxelib run duell build html5 -test -verbose -D jenkins -yestoall
 
 haxelib run duell build flash -test -verbose -D jenkins -yestoall
 
 haxelib run duell build ios -test -verbose -D jenkins -yestoall
+
+haxelib run duell build android -test -verbose -D jenkins -yestoall
