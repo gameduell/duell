d749867da8d59c653cc141548e9f0716c968a898
Sven Otto <sven.otto@gameduell.de>
2015-05-05 12:17:38 +0200

    Preparation for open source. Added license header. Added README.md. Updated haxelib.jsom

---
+++ README.md
@@ -0,0 +1,18 @@
+= Description =
+
+The Duell Tool is a command line tool for setting up, building and running applications on any platform.
+It can also be used for other general tasks within app development. It is made in a modular way via plugins,
+and each plugin is separate from the tool itself. The tool merges the needed plugin on execution.
+
+
+The execution of the tool is always "duell" followed by a command such as "build" or "setup" and
+then a plugin name like "ios" or "emptyProject". For example, "duell build ios" will run the command
+"build" with the "ios" plugin. You can also pass additional parameters to
+execution by prefixing with "-", e.g. "duell build ios -verbose".
+
+
+= Release Log =
+
+== 0.0.1 ==
+
+- Initial setup for github
--- duell/Duell.hx
+++ duell/Duell.hx
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2003-2015, GameDuell GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package duell;
 
 import haxe.io.Path;
@@ -18,13 +44,6 @@ import duell.commands.CreateCommand;
 import duell.commands.EnvironmentSetupCommand;
 import duell.commands.ToolSetupCommand;
 
-
-
-/**
- * @autor kgar
- * @date 24.06.2014.
- * @company Gameduell GmbH
- */
 class Duell 
 {
     public static var VERSION = "1.0.1";
