c229c36aebeffdcf5326f422784fa9a7bf40894b
Rui Campos <ruir2c@gmail.com>
2015-05-18 18:09:14 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -6,17 +6,17 @@ mkdir test
 
 cd test
 
-haxelib run duell create unitTestProject -verbose -yestoall
+haxelib run duell_duell create unitTestProject -verbose -yestoall
 
 #because the unitTestProject sample has a specified duell tool version
 sed -i '' 's/supported-duell-tool version=".*"/supported-duell-tool version="master"/g' duell_project.xml
 
-haxelib run duell update -verbose -yestoall
+haxelib run duell_duell update -verbose -yestoall
 
-haxelib run duell build html5 -test -verbose -D jenkins -yestoall
+haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 
-haxelib run duell build flash -test -verbose -D jenkins -yestoall
+haxelib run duell_duell build flash -test -verbose -D jenkins -yestoall
 
-haxelib run duell build ios -simulator -test -verbose -D jenkins -yestoall
+haxelib run duell_duell build ios -simulator -test -verbose -D jenkins -yestoall
 
-haxelib run duell build android -emulator -x86 -test -verbose -D jenkins -yestoall
+haxelib run duell_duell build android -emulator -x86 -test -verbose -D jenkins -yestoall
