dec4b726bbc092ce568f0b94dcfe4119f2c2cdbe
kgar <khaled.garbaya@gameduell.com>
2015-09-17 11:13:34 +0200

    now using python urllib to download

--- duell/helpers/DownloadHelper.hx
+++ duell/helpers/DownloadHelper.hx
@@ -33,65 +33,7 @@ import sys.io.File;
 import haxe.io.Path;
 import haxe.Http;
 
-class Progress extends haxe.io.Output
-{
-	var o : haxe.io.Output;
-	var cur : Int;
-	var max : Int;
-	var start : Float;
-
-	public function new(o)
-	{
-		this.o = o;
-		cur = 0;
-		start = haxe.Timer.stamp();
-	}
-
-
-	override function writeByte(c)
-	{
-		o.writeByte(c);
-		bytes(1);
-	}
-
-	override function writeBytes(s, p, l)
-	{
-		var r = o.writeBytes(s,p,l);
-		bytes(r);
-		return r;
-	}
-
-	override function close()
-	{
-		super.close();
-		o.close();
-		var time = haxe.Timer.stamp() - start;
-		var speed = (cur / time) / 1024;
-		time = Std.int(time * 10) / 10;
-		speed = Std.int(speed * 10) / 10;
-
-		// When the path is a redirect, we don't want to display that the download completed
-
-		if (cur > 400)
-		{
-			Sys.print("Download complete : " + cur + " bytes in " + time + "s (" + speed + "KB/s)\n");
-		}
-	}
-
-	override function prepare(m)
-	{
-		max = m;
-	}
-
-	private function bytes(n)
-	{
-		cur += n;
-		if( max == null )
-			Sys.print(cur+" bytes\r");
-		else
-			Sys.print(cur+"/"+max+" ("+Std.int((cur*100.0)/max)+"%)\r");
-	}
-}
+import python.urllib.Request;
 
 class DownloadHelper
 {
@@ -116,33 +58,10 @@ class DownloadHelper
 			FileSystem.deleteFile(localPath);
 		}
 
-		var out = File.write(localPath, true);
-		var progress = new Progress(out);
-		var h = new Http(remotePath);
 
-		h.cnxTimeout = 30;
-
-		h.onError = function (e) {
-			progress.close();
-			FileSystem.deleteFile(localPath);
-			throw e;
-		};
-
-		if (!followingLocation)
-		{
-			Sys.println("Downloading " + localPath + "...");
-		}
-
-		h.customRequest (false, progress);
-
-		if (h.responseHeaders != null && h.responseHeaders.exists("Location"))
-		{
-			var location = h.responseHeaders.get("Location");
-
-			if (location != remotePath)
-			{
-				downloadFile (location, localPath, true);
-			}
-		}
+		Request.urlretrieve(remotePath, localPath, function(blocknr, blocksize, size){
+			var current = blocknr*blocksize;
+			Sys.print(Std.string(current)+"/"+Std.string(size)+" ("+Std.int((current*100.0)/size)+"%)\r");
+		});
 	}
 }
